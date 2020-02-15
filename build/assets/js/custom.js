50da6928bbee817eafd021a37c68e7ff60f1af8c
ejhb <bonurajosh13@gmail.com>
2020-02-15 20:19:06 +0100

    add_error.php | error-404.php | robots-https.txt |

--- build/assets/js/custom.js
+++ build/assets/js/custom.js
@@ -62,27 +62,27 @@
 		$('.mu-skill-progress-bar').appear(function() {
 
 		 	$('.mu-html5-bar').LineProgressbar({
-				percentage: 95,
+				percentage: 65,
 				triggerOnce: true
 			});
 
 			$('.mu-css-bar').LineProgressbar({
-				percentage: 90,
+				percentage: 70,
 				triggerOnce: true
 			});
 
 			$('.mu-photoshop-bar').LineProgressbar({
-				percentage: 85,
-				triggerOnce: true
-			});
-
-			$('.mu-wordpress-bar').LineProgressbar({
 				percentage: 80,
 				triggerOnce: true
 			});
 
+			$('.mu-wordpress-bar').LineProgressbar({
+				percentage: 90,
+				triggerOnce: true
+			});
+
 			$('.mu-jquery-bar').LineProgressbar({
-				percentage: 55,
+				percentage: 85	,
 				triggerOnce: true
 			});
 
